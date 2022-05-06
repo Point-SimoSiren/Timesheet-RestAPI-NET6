dd42720f3f0b7f6bde3d3123f2e0daf05c8331ab
Simo Siren <sirensimo7@gmail.com>
2022-05-06 10:49:12 +0300

    Add project files.

---
+++ TimesheetBackend2022k/Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimesheetBackend2022k.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+    }
+}
---
+++ TimesheetBackend2022k/Models/Timesheet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimesheetBackend2022k.Models
+{
+    public partial class Timesheet
+    {
+        public int IdTimesheet { get; set; }
+        public int IdCustomer { get; set; }
+        public int IdEmployee { get; set; }
+        public int IdWorkAssignment { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? StopTime { get; set; }
+        public string? Comments { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public bool? Active { get; set; }
+        public string? StopLatitude { get; set; }
+        public string? StopLongitude { get; set; }
+        public string? StartLatitude { get; set; }
+        public string? StartLongitude { get; set; }
+
+        public virtual Customer IdCustomerNavigation { get; set; } = null!;
+        public virtual Employee IdEmployeeNavigation { get; set; } = null!;
+        public virtual WorkAssignment IdWorkAssignmentNavigation { get; set; } = null!;
+    }
+}
